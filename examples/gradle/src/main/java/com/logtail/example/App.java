324684bc2667105b0bb5f9404dbafa26984de508
Andrej Hoos <andrej.hoos@gmail.com>
2022-10-12 15:44:42 +0200

    Add sleep to the Gradle example

--- examples/gradle/src/main/java/com/logtail/example/App.java
+++ examples/gradle/src/main/java/com/logtail/example/App.java
@@ -3,9 +3,17 @@ package com.logtail.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.TimeUnit;
+
 public class App {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(App.class);
         logger.info("hello world");
+
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
