06619593ca3850f8a08102c05df4218daf3a1942
Petr Heinz <petr@betterstack.com>
2023-07-20 11:16:43 +0200

    Use UUID in requestId context field

--- examples/maven/src/main/java/com/logtail/example/App.java
+++ examples/maven/src/main/java/com/logtail/example/App.java
@@ -5,12 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
 import java.time.Instant;
+import java.util.UUID;
 
 public class App {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(App.class);
 
-        MDC.put("requestId", "stringValue");
+        MDC.put("requestId", UUID.randomUUID().toString());
         MDC.put("requestTime", Long.toString(Instant.now().getEpochSecond()));
 
         logger.info("hello world");
