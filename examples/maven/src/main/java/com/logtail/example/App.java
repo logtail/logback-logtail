1bb9241dc801231acacb7eeaf70810504dadc008
Petr Heinz <petr@betterstack.com>
2023-07-20 11:11:19 +0200

    Use context fields in Maven example project

--- examples/maven/src/main/java/com/logtail/example/App.java
+++ examples/maven/src/main/java/com/logtail/example/App.java
@@ -2,10 +2,17 @@ package com.logtail.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.time.Instant;
 
 public class App {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(App.class);
+
+        MDC.put("requestId", "stringValue");
+        MDC.put("requestTime", Long.toString(Instant.now().getEpochSecond()));
+
         logger.info("hello world");
     }
 }
