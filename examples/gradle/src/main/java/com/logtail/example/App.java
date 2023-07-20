524ae8a0822bf5237cd0a94bff439565db7b9eb5
Petr Heinz <petr@betterstack.com>
2023-07-20 11:10:19 +0200

    Use context fields in Gradle example project

--- examples/gradle/src/main/java/com/logtail/example/App.java
+++ examples/gradle/src/main/java/com/logtail/example/App.java
@@ -2,12 +2,18 @@ package com.logtail.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
+import java.time.Instant;
 import java.util.concurrent.TimeUnit;
 
 public class App {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(App.class);
+
+        MDC.put("requestId", "stringValue");
+        MDC.put("requestTime", Long.toString(Instant.now().getEpochSecond()));
+
         logger.info("hello world");
 
         try {
